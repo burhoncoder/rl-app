3a369b9131d37835271c84ff139bbe41dbab22c0
Burkhon <megacoder77@gmail.com>
2025-02-24 01:14:26 +0500

    feat: add docs to controller method getLocus

--- src/modules/locus/locus.controller.ts
+++ src/modules/locus/locus.controller.ts
@@ -1,4 +1,6 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
+
 import { LocusService } from './app/locus.service';
 import { GetLocusQueryDto } from './app/get-locus-query.dto';
 
@@ -6,6 +8,11 @@ import { GetLocusQueryDto } from './app/get-locus-query.dto';
 export class LocusController {
   constructor(private locusService: LocusService) {}
 
+  @ApiOperation({
+    summary: 'Get paginated list of locus',
+    description:
+      'Retrieve a paginated list of loci with the option to filter and sort the results.',
+  })
   @Get()
   getLocus(@Query() getLocusQuery: GetLocusQueryDto) {
     return this.locusService.getLocus(getLocusQuery);
